62302497127d8f955a6cb0ab38238ca7d81810cc
sebawita <sebawita@gmail.com>
2016-02-19 16:22:28 +0000

    Going to the beginning
    Removing code that is expected to be created with code changes in the
    workshop

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,38 +1,11 @@
 import {Component} from "angular2/core";
-import {Router} from "angular2/router";
-
-import {User} from "../../shared/user/user";
-import {UserService} from "../../shared/user/user.service";
-import {NS_ROUTER_DIRECTIVES} from "nativescript-angular/router";
 
 @Component({
   selector: "login",
-  templateUrl: "pages/login/login.html",
-  providers: [UserService],
-  directives: [NS_ROUTER_DIRECTIVES]
+  template: `
+    <Label text="Hello World"></Label>
+  `
 })
 export class LoginPage {
-  user: User;
-
-  constructor(
-    private _router: Router,
-    private _userService: UserService) {
-
-    this.user = new User();
-
-    // Hardcode a few values to make testing easy
-    this.user.email = "nativescriptrocks@telerik.com";
-    this.user.password = "password";
-  }
-
-  signIn() {
-    this._userService.login(this.user)
-      .subscribe(
-      () => this._router.navigate(["List"]),
-      (error) => {
-        console.log(error);
-        alert("Login error");
-      }
-      );
-  }
+  
 }
